c3ccf4f5b6fc83e469e164d30bcddd4b4d272511
KalpKJ <kalpjoshi2003@gmail.com>
2024-12-20 00:05:46 -0600

    connected the file with database MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
+const express = require("express"); //to start localhost server
+const app = express(); //storing it in a variable
+const mongoose = require("mongoose"); //required to interact with Database
 
+//this url that you get from MongoDB website
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+//calling the main function
+main()
+//then method for promises
+.then(() =>{
+    console.log("connected to DB"); 
+})
+//catching any errors
+.catch((err) =>{
+    console.log(err);
+});
+
+//an async function to connect your JS file with MongoDB
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+//root API
 app.get("/", (req,res) =>{
     res.send("Hi I am root");
 });
 
+//making the server listen to port 8080
 app.listen(8080, () =>{
     console.log("server is listening to port 8080");
 })
