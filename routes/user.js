d5721d40c97b691e53a13d4f0309f31924276a06
KalpKJ <kalpjoshi2003@gmail.com>
2024-12-30 17:03:29 -0600

    added auto login after signup

--- routes/user.js
+++ routes/user.js
@@ -7,20 +7,28 @@ router.get("/signup", (req, res) =>{
     res.render("users/signup.ejs");
 });
 
-router.post("/signup", wrapAsync(async(req, res) =>{
-    try{
-        let {username, email, password} = req.body;
-    const newUser = new User({email, username});
-    const registeredUser = await User.register(newUser, password);
-    console.log(registeredUser);
-    req.flash("success", "Welcome to WanderLust");
-    res.redirect("/listings");
-    }catch(e){
-        req.flash("error", e.message);
-        res.redirect("/signup");
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.login(registeredUser, (err) =>{
+        if(err){
+          return next(err);
+        }
+        req.flash("success", "Welcome to WanderLust");
+        res.redirect("/listings");
+      });
+      
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
     }
-    
-}));
+  })
+);
 
 router.get("/login", (req, res) => {
   res.render("users/login.ejs");
